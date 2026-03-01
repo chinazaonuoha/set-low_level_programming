b346d59ec65a94af84222e5cec43b91be4616cdc
chinazaonuoha <developerworkpace@gmail.com>
2026-03-01 08:46:23 +0100

    Betty style warning: line character fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,9 +15,9 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 last_digit = (n % 10);
 /* your code goes there */
-if (last_digit < 6 && last_digit != 0)
+if (last_digit < 6 && last_digit < 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0 \n", n, last_digit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
 else if (last_digit == 0)
 {
@@ -25,7 +25,7 @@ printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 else
 {
-printf("Last digit of %d is %d and is greater than 5 \n", n, last_digit);
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 return (0);
 }
