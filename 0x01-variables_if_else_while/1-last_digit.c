6f8f80f8e43aedbb6043db8933875443c40d0114
chinazaonuoha <developerworkpace@gmail.com>
2026-03-01 08:29:28 +0100

    Assign a random number to the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int n;
 int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-last_digit = abs(n % 10);
+last_digit = (n % 10);
 /* your code goes there */
 if (last_digit < 6 && last_digit != 0)
 {
