6393bfe1de623b89de825df21c2bfefd6751a4e1
chinazaonuoha <developerworkpace@gmail.com>
2026-03-18 08:39:02 +0100

    Write a function that prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * _putchar - prints a character to the console
+ * @c: the character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned.
+ */
 int _putchar(char c);
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+void print_alphabet(void);
 
 #endif
