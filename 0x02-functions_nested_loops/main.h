119940b103fb9c8863a0ee7c68e11062fc1cc8cf
chinazaonuoha <developerworkpace@gmail.com>
2026-03-19 16:36:48 +0100

    Write a function that prints the 9 times table, starting with 0.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -64,4 +64,9 @@ int print_last_digit(int);
  * jack_bauer - prints every minute of the day of Jack Bauer
  */
 void jack_bauer(void);
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void);
 #endif
