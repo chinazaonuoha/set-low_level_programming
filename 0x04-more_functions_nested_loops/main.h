4cf08953df6f671aa9c19abb2f45726e413d78c1
chinazaonuoha <developerworkpace@gmail.com>
2026-03-21 02:50:30 +0100

    Write a function that prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -38,4 +38,9 @@ void print_numbers(void);
  * print_most_numbers - prints the numbers from 0 to 9 except 2 and 4
  */
 void print_most_numbers(void);
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ */
+void more_numbers(void);
 #endif
