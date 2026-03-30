d5ede68f30f85615243d26563b288322aaaf6d06
chinazaonuoha <developerworkpace@gmail.com>
2026-03-30 12:18:37 +0100

    Write a function that swaps the values of two integers.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * reset_to_98 - resets the value of an integer to 98
+ * @n: pointer to the integer to be reset
+ */
 void reset_to_98(int *n);
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ */
+void swap_int(int *a, int *b);
 
 #endif /* MAIN_H */
