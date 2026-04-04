4cbf336755325dead45bb189d73c85017c6070f6
chinazaonuoha <developerworkpace@gmail.com>
2026-04-04 22:46:54 +0100

    Write a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,24 +11,19 @@ char *leet(char *str)
 int i = 0;
 while (str[i] != '\0')
 {
+if (str[i] == 'a' || str[i] == 'A' || str[i] == 'e' || str[i] == 'E'
+|| str[i] == 'o' || str[i] == 'O'
+|| str[i] == 't' || str[i] == 'T' || str[i] == 'l' || str[i] == 'L')
+{
 if (str[i] == 'a' || str[i] == 'A')
-{
 str[i] = '4';
-}
 else if (str[i] == 'e' || str[i] == 'E')
-{
 str[i] = '3';
-}
 else if (str[i] == 'o' || str[i] == 'O')
-{
 str[i] = '0';
-}
 else if (str[i] == 't' || str[i] == 'T')
-{
 str[i] = '7';
-}
 else if (str[i] == 'l' || str[i] == 'L')
-{
 str[i] = '1';
 }
 i++;
