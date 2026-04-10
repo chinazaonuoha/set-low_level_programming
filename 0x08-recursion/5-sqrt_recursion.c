44b93d853fa7c9a1264ab84e3ca308d8668e2358
chinazaonuoha <developerworkpace@gmail.com>
2026-04-10 12:51:36 +0100

    Write a function that returns the natural square root of a number.

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to calculate the square root of
+ * Return: the natural square root of n, or
+ * -1 if n does not have a natural square root
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 0 || n == 1)
+return (n);
+return (_sqrt_helper(n, 1));
+}
+
+
+/**
+ * _sqrt_helper - helper function to calculate the natural
+ * square root of a number
+ * @n: the number to calculate the square root of
+ * @i: the current guess for the square root
+ * Return: the natural square root of n, or -1 if n
+ * does not have a natural square root
+ */
+int _sqrt_helper(int n, int i)
+{
+if (i * i == n)
+return (i);
+if (i * i > n)
+return (-1);
+return (_sqrt_helper(n, i + 1));
+}
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -96,4 +96,18 @@ unsigned int _strspn(char *s, char *accept);
  */
  int _pow_recursion(int x, int y);
 
+ /** _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to calculate the square root of
+ * Return: the natural square root of n, or -1 if n does not have a natural square root
+ */
+ int _sqrt_recursion(int n);
+
+ /**
+  * _sqrt_helper - helper function to calculate the natural square root of a number
+  * @n: the number to calculate the square root of
+  * @i: the current guess for the square root
+  * Return: the natural square root of n, or -1 if n does not have a natural square root
+  */
+ int _sqrt_helper(int n, int i);
+
 #endif /* MAIN_H */ 
