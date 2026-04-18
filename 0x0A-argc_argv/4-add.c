faf44554ae78ef1d8e6886108625da7d2031d514
chinazaonuoha <developerworkpace@gmail.com>
2026-04-18 17:28:26 +0100

    Write a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * is_number - checks if a string contains only digits
+ * @str: input string
+ * Return: 1 if valid number, 0 otherwise
+ */
+int is_number(char *str)
+{
+int i = 0;
+
+if (str[0] == '\0')
+return (0);
+
+while (str[i])
+{
+if (!isdigit(str[i]))
+return (0);
+i++;
+}
+return (1);
+}
 
 /**
  * main - adds all numbers passed as arguments
@@ -17,13 +39,18 @@ printf("Error\n");
 return (1);
 }
 
-i = 1;
-while (i < argc)
+for (i = 1; i < argc; i++)
 {
+if (!is_number(argv[i]))
+{
+printf("Error\n");
+return (1);
+}
+
 sum += atoi(argv[i]);
-i++;
 }
 
 printf("%d\n", sum);
 return (0);
 }
+
