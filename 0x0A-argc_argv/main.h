c3d6949d460055830fc3c4a97b2e3b55a367a279
chinazaonuoha <developerworkpace@gmail.com>
2026-04-18 04:32:55 +0100

    Write a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings representing the command-line arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[]);
+
+/* _putchar - Writes a character to stdout
+ * @c: The character to print
+ * Return: On success, the character printed. On error, EOF is returned.
+ */
+int _putchar(char c);
+
+#endif /* MAIN_H */
