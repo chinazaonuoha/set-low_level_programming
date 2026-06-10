ab51af6a9ea6454e2606359a6dc17535c256d038
chinazaonuoha <developerworkpace@gmail.com>
2026-06-10 09:49:25 +0100

    Write a function that concatenates two strings.

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -16,4 +16,12 @@ char *create_array(unsigned int size, char c);
  * Return: A pointer to the duplicated string, or NULL on failure
  */
 char *_strdup(char *str);
+
+/**
+ * str_concat - Concatenates two strings by allocating memory and copying the content
+ * @s1: The first string to concatenate
+ * @s2: The second string to concatenate
+ * Return: A pointer to the concatenated string, or NULL on failure
+ */
+char *str_concat(char *s1, char *s2);
 #endif /* MAIN_H */
