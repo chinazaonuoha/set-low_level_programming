2bb287a498ebc261ab0929618f352f5888064587
chinazaonuoha <developerworkpace@gmail.com>
2026-06-13 18:42:46 +0100

    Write a function that adds a new node at the end of a list_t list.

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/** * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the head of the list
+ * @n: string to be added
+ * Return: pointer to the new node, or NULL on failure
+ */
+
+list_t *add_node_end(list_t **head, const char *n)
+{
+list_t *new_node;
+list_t *current;
+
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
+
+new_node->str = strdup(n);
+new_node->next = NULL;
+
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
+
+current = *head;
+while (current->next != NULL)
+current = current->next;
+
+current->next = new_node;
+return (new_node);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -43,5 +43,22 @@ size_t list_len(const list_t *h);
  */
 list_t *add_node(list_t **head, const char *str);
 
+/**
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the head of the list
+ * @str: string to be added (malloc'ed string)
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+list_t *add_node_end(list_t **head, const char *str);
+
+/**
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the head of the list
+ * @str: string to be added (malloc'ed string)
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif /* LISTS_H */
