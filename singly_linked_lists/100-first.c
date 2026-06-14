219bb1ee913add2afa4f68190488c694bccac71a
chinazaonuoha <developerworkpace@gmail.com>
2026-06-14 10:29:43 +0100

    Write a function that prints before the main function is executed.

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * pre_main_message - Executed automatically before main()
+ *
+ * Description: Uses a compiler attribute to force execution during
+ * the runtime initialization phase.
+ */
+void __attribute__((constructor)) pre_main_message(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore, my house upon my back!\n");
+}
