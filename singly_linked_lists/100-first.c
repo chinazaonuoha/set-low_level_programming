6798c7955ae9b15d89e2082fa03a0435b0be3d69
chinazaonuoha <developerworkpace@gmail.com>
2026-06-14 10:31:59 +0100

    Write a function that prints before the main function is executed.

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -9,5 +9,5 @@
 void __attribute__((constructor)) pre_main_message(void)
 {
 printf("You're beat! and yet, you must allow,\n");
-printf("I bore, my house upon my back!\n");
+printf("I bore my house upon my back!\n");
 }
