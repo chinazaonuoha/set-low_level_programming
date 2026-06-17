cbab2e347d2484e00f6fc675c83c7f395fbe51dc
chinazaonuoha <developerworkpace@gmail.com>
2026-06-17 15:15:41 +0100

    Write a function that prints all the elements of a dlistint_t list.

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,58 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * struct dlistint_s - Doubly linked list node structure
+ * @n: Integer data stored in the node
+ * @prev: Pointer to the previous node in the list
+ * @next: Pointer to the next node in the list
+ *
+ * Description: This structure represents a node in a doubly linked list.
+ */
+typedef struct dlistint_s
+{
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
+} dlistint_t;
+
+/**
+ * print_dlistint - Prints all the elements of a doubly linked list
+ * @h: Pointer to the head of the list
+ *
+ * Return: The number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h);
+
+/**
+ * dlistint_len - Returns the number of elements in a doubly linked list
+ * @h: Pointer to the head of the list
+ *
+ * Return: The number of nodes in the list
+ */
+size_t dlistint_len(const dlistint_t *h);
+
+/**
+ * add_dnodeint - Adds a new node at the beginning of a doubly linked list
+ * @head: Double pointer to the head of the list
+ * @n: Integer data for the new node
+ *
+ * Return: Pointer to the new node, or NULL if allocation fails
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+/**
+ * add_dnodeint_end - Adds a new node at the end of a doubly linked list
+ * @head: Double pointer to the head of the list
+ * @n: Integer data for the new node
+ *
+ * Return: Pointer to the new node, or NULL if allocation fails
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+/**
+ * free_dlistint - Frees all nodes in a doubly linked list
+ * @head: Pointer to the head of the list
+ */
+void free_dlistint(dlistint_t *head);
+#endif /* LISTS_H */
