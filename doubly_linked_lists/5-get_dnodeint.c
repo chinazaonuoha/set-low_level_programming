3e21bd88363a73afa735b9633e279550d8f0186d
chinazaonuoha <developerworkpace@gmail.com>
2026-06-17 19:39:21 +0100

    Write a function that returns the nth node of a dlistint_t linked list.

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -12,7 +12,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 dlistint_t *current = head;
 unsigned int i;
-
 for (i = 0; i < index && current != NULL; i++)
 {
 current = current->next;
