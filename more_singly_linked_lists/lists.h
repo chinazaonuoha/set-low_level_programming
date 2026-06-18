692ca435b2babdc496ef35a563bd210ca7bb1973
chinazaonuoha <developerworkpace@gmail.com>
2026-06-18 16:01:42 +0100

    Write a function that returns the sum of all the data (n) of a listint_t linked list.

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -4,34 +4,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct list_s
+
+typedef struct listint_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
 /**
  * sum_listint - returns the sum of all the data (n) of a listint_t linked list.
  * @head: pointer to the head of the listint_t linked list
  * Return: the sum of all the data (n) of a listint_t linked list, or 0 if the list is empty
  */
-int sum_listint(list_t *head);
+int sum_listint(listint_t *head);
 
 
 /**
  * free_listint2 - frees a listint_t list and sets the head to NULL.
  * @head: pointer to a pointer to the head of the listint_t list.
  */
-void free_listint2(list_t **head);
+void free_listint2(listint_t **head);
 
 
 /**
@@ -41,6 +33,6 @@ void free_listint2(list_t **head);
  *
  * Return: a pointer to the new node.
  */
-list_t *add_nodeint_end(list_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 
 #endif /* LISTS_H */
