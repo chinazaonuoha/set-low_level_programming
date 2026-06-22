6451cd3199a2a187387bb57069c8d84ecd36845b
chinazaonuoha <developerworkpace@gmail.com>
2026-06-22 11:08:49 +0100

    Write a function that creates a hash table.

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -2,6 +2,13 @@
 #define HASH_TABLES_H
 
 
+/**
+ * struct hash_node_s - Node of a hash table.
+ *
+ * @key: The key, string. The key is unique in the HashTable.
+ * @value: The value corresponding to a key.
+ * @next: A pointer to the next node of the List.
+ */
 typedef struct hash_node_s
 {
 char *key;
@@ -9,6 +16,14 @@ char *value;
 struct hash_node_s *next;
 } hash_node_t;
 
+/**
+ * struct hash_table_s - Hash table data structure.
+ *
+ * @size: The size of the array.
+ * @array: An array of size @size. Each cell is a pointer
+ * to the first node of a linked list, because we want our
+ * hash table to use a Chaining collision handling.
+ */
 typedef struct hash_table_s
 {
 unsigned long int size;
