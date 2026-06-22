8c6edb162dd579f08f919bb57cf72b2d3cf6fa47
chinazaonuoha <developerworkpace@gmail.com>
2026-06-22 20:49:03 +0100

    Write a function that prints a hash table.

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -2,24 +2,33 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - Prints a hash table.
- * @ht: The hash table.
+ * hash_table_print - Prints a hash table in Python dictionary format.
+ * @ht: A pointer to the hash table instance to print.
  */
 void hash_table_print(const hash_table_t *ht)
 {
 unsigned long int i;
 hash_node_t *cursor = NULL;
+int comma_flag = 0;
 
 if (ht == NULL)
 return;
 
+printf("{");
+
 for (i = 0; i < ht->size; i++)
 {
 cursor = ht->array[i];
+
 while (cursor != NULL)
 {
-printf("'%s': '%s'\n", cursor->key, cursor->value);
+if (comma_flag == 1)
+printf(", ");
+
+printf("'%s': '%s'", cursor->key, cursor->value);
+comma_flag = 1;
 cursor = cursor->next;
 }
 }
+printf("}\n");
 }
